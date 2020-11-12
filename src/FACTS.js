b92e7020a840875cdd9ca32d4665351e1dafaabf
felicianoWin <me@weberfelix.com>
2020-11-12 16:30:38 +0100

    Edit facts list

--- src/FACTS.js
+++ src/FACTS.js
@@ -1,8 +1,8 @@
 exports.FACTS = [
-    "My Mandarin Chinese is pretty good, I lived in China for 4 years",
-    "I speak Italian, German and Eglish fluently",
+    "I speak Italian, German and English fluently",
     "I love to draw, and I'm working on my animation skills",
-    "I like to take pictures, have a look at my gallery :)",
-    "Hacker at heart",
-    "I started coding when Ruby on Rails was all thy hype"
+    "I studied Chinese and lived in China for 4 years ",
+    "I like to take pictures, have a look at my <a href='http://photo.weberfelix.com'>gallery <a>:) the code for the gallery <a href='https://github.com/felicianodev'> is here </a>",
+    "I'm a hacker at heart.",
+    "You can have a look at the code for this website <a href='https://github.com/felicianodev'> here</a>."
 ]
